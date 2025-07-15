6f444fabf5c7a43141d8317f1d94c8f7110de6d2
Tariq-Monowar <hassainseam@gmail.com>
2025-07-15 16:22:34 +0600

    transfer image to uploade folder ---

--- module/v1/products/products.controllers.ts
+++ module/v1/products/products.controllers.ts
@@ -933,7 +933,7 @@ export const characteristicsIcons = async (req: Request, res: Response) => {
   try {
     const iconsWithUrls = data.map((icon) => ({
       ...icon,
-      image: getImageUrl(`/assets/${icon.image}`),
+      image: getImageUrl(`/uploads/${icon.image}`),
     }));
 
     res.status(200).json({
