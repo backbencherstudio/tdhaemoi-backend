22f47a5a9da7878ec66941af2f2c0d5a8cd18fd5
Tariq-Monowar <hassainseam@gmail.com>
2025-07-31 16:32:14 +0600

    versorgungen update

--- module/v1/versorgungen/versorgungen.controllers.ts
+++ module/v1/versorgungen/versorgungen.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import { log } from "console";
 
 const prisma = new PrismaClient();
 
@@ -61,3 +62,48 @@ export const createVersorgungen = async (req: Request, res: Response) => {
     });
   }
 };
+
+
+
+export const patchVersorgungen = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const existingVersorgungen = await prisma.versorgungen.findUnique({
+      where: { id },
+    });
+
+    if (!existingVersorgungen) {
+      return res.status(404).json({
+        success: false,
+        message: "Versorgungen not found",
+      });
+    }
+
+    const updatedVersorgungenData = Object.fromEntries(
+      Object.entries(req.body).filter(([key, value]) => value !== undefined)
+    );
+
+    const updatedVersorgungen = await prisma.versorgungen.update({
+      where: { id },
+      data: updatedVersorgungenData,
+    });
+
+    console.log(updatedVersorgungenData);  
+
+
+    res.status(200).json({
+      success: true,
+      message: "Versorgungen updated successfully",
+      versorgungen: updatedVersorgungen,
+    });
+  } catch (error) {
+    console.error("Patch Versorgungen error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+};
+
--- module/v1/versorgungen/versorgungen.routes.ts
+++ module/v1/versorgungen/versorgungen.routes.ts
@@ -2,11 +2,11 @@ import express from "express";
 
 
 import { verifyUser } from "../../../middleware/verifyUsers";
-import { createVersorgungen } from "./versorgungen.controllers";
+import { createVersorgungen, patchVersorgungen } from "./versorgungen.controllers";
 
 const router = express.Router();
 
 router.post("/", verifyUser("PARTNER"), createVersorgungen);
-
+router.patch("/:id", verifyUser("PARTNER"), patchVersorgungen);
 
 export default router;
