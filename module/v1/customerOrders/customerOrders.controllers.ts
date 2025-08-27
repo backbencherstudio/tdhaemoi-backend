f96e4f10c22f5fe37c4cb93e21b48a9141004a8a
Tariq-Monowar <hassainseam@gmail.com>
2025-08-27 17:02:23 +0600

    fix OrderStatus & add statusUpdate

---
+++ module/v1/customerOrders/customerOrders.controllers.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import fs from "fs";
+import iconv from "iconv-lite";
+import csvParser from "csv-parser";
+import { getImageUrl } from "../../../utils/base_utl";
+import path from "path";
+
+const prisma = new PrismaClient();
+
+export const createOrder = async (req: Request, res: Response) => {
+  try {
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+};
--- module/v1/customerOrders/customerOrders.routes.ts
+++ module/v1/customerOrders/customerOrders.routes.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { verifyUser } from "../../../middleware/verifyUsers";
 
+import { createOrder } from "./customerOrders.controllers";
+
 const router = express.Router();
+
+router.get("/create-order", verifyUser("ADMIN", "PARTNER"), createOrder );
 
 export default router;
