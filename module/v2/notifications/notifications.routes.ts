563b5029382bb3aaa5e9549b8680c14bae098282
Tariq-Monowar <hassainseam@gmail.com>
2025-12-24 22:33:23 +0600

    update

--- module/v2/notifications/notifications.routes.ts
+++ module/v2/notifications/notifications.routes.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import { verifyUser } from "../../../middleware/verifyUsers";
-import { createNotification, getAllNotificaions, getCountUnreadNotifications, markeAsReadNotifications } from "./notifications.controllers";
+import {
+  createNotification,
+  getAllNotificaions,
+  getCountUnreadNotifications,
+  markeAsReadNotifications,
+  deleteNotifications
+} from "./notifications.controllers";
 
 const router = express.Router();
 
@@ -8,7 +14,16 @@ router.post("/create", verifyUser("PARTNER", "ADMIN"), createNotification);
 
 router.get("/get-all", verifyUser("PARTNER", "ADMIN"), getAllNotificaions);
 
-router.get("/unread-count", verifyUser("PARTNER", "ADMIN"), getCountUnreadNotifications);
-router.patch("/mark-as-read", verifyUser("PARTNER", "ADMIN"), markeAsReadNotifications);
+router.get(
+  "/unread-count",
+  verifyUser("PARTNER", "ADMIN"),
+  getCountUnreadNotifications
+);
+router.patch(
+  "/mark-as-read",
+  verifyUser("PARTNER", "ADMIN"),
+  markeAsReadNotifications
+);
+router.delete("/delete", verifyUser("PARTNER", "ADMIN"), deleteNotifications);
 
 export default router;
